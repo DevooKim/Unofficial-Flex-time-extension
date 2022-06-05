64d797261d5db43c6504284e7813936f4cda1fec
Kim Hyun Woo <42219589+DevooKim@users.noreply.github.com>
2022-06-06 01:47:26 +0900

    fix: 오타수정

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -8,7 +8,7 @@ const validateSender = (
     sender: chrome.runtime.MessageSender
 ) => sender.id === chrome.runtime.id && message.from === Sender.React;
 
-const messgesFromReactAppListener = (
+const messagesFromReactAppListener = (
     message: ChromeMessage,
     sender: chrome.runtime.MessageSender,
     sendResponse: MessageResponse
@@ -17,15 +17,15 @@ const messgesFromReactAppListener = (
 
     if (isValidate && message.message === "parseTime") {
         const timeList: TimeList[] = parseTime();
-        const calcedTime: ResultData = calculateTime(timeList);
-        sendResponse(JSON.stringify(calcedTime));
+        const calculatedTime: ResultData = calculateTime(timeList);
+        sendResponse(JSON.stringify(calculatedTime));
         // response가 단순 비동기인 경우 return true; => 가비지 컬렉터
     }
 };
 
 const main = () => {
     console.log("[content.ts] Main");
-    chrome.runtime.onMessage.addListener(messgesFromReactAppListener);
+    chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
 };
 
 main();
