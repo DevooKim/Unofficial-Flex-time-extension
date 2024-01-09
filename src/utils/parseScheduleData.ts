b1407e6fc5a74bb9e7593b58267fe6e7006e7d46
Devookim <khwoo8026@gmail.com>
2024-01-10 00:40:10 +0900

    fix: 근무일 계산 수정

--- src/utils/parseScheduleData.ts
+++ src/utils/parseScheduleData.ts
@@ -156,7 +156,8 @@ export const parseScheduleData = ({
     const 남은평균근무시간 = 남은근무시간 / 남은근무일 || 0
 
     const 남은근무일_지금기준 =
-        clockData.현재근무상태 === '출근전' ? 남은근무일 - 1 : 남은근무일
+        clockData.현재근무상태 !== '출근전' ? 남은근무일 - 1 : 남은근무일
+
     const 남은근무시간_지금기준 =
         clockData.현재근무상태 === '근무중'
             ? 남은근무시간 - clockData.오늘일한시간
