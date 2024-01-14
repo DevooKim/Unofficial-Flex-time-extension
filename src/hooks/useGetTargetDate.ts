b31430fd8164c0e8252b30389a909eb4675646a9
Devookim <khwoo8026@gmail.com>
2024-01-14 22:55:59 +0900

    refactor: 월 계산 로직 개선

--- src/hooks/useGetTargetDate.ts
+++ src/hooks/useGetTargetDate.ts
@@ -1,55 +1,15 @@
-import { useEffect, useState } from 'react'
-import { activeTabHandler } from '../chrome/utils'
+import { useState } from 'react'
 import dayjs, { Dayjs } from 'dayjs'
-
-const getTimeStampFromQuery = (tab: chrome.tabs.Tab): string | null => {
-    const url = tab.url || ''
-    const queryString = url.split('?')
-    const UrlSearch = new URLSearchParams(queryString[1])
-    const ts = UrlSearch.get('ts')
-
-    return ts
-}
 
 const useGetTargetDate = () => {
     const [targetDate, setTargetDate] = useState(dayjs())
     const [targetTimeStamp, setTargetTimeStamp] = useState<string>(
         dayjs().endOf('month').valueOf().toString()
     )
-
-    const setDateByTimestamp = (timestamp: string) => {
-        setTargetTimeStamp(timestamp)
-        setTargetDate(dayjs(parseInt(timestamp as string, 10)))
-    }
-
     const setDateByDayjs = (day: Dayjs) => {
         setTargetDate(day)
         setTargetTimeStamp(day.valueOf().toString())
     }
-
-    useEffect(() => {
-        chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-            activeTabHandler(tab, ({ isComplete }) => {
-                if (isComplete) {
-                    const ts = getTimeStampFromQuery(tab)
-                    if (ts) {
-                        setDateByTimestamp(ts)
-                    }
-                }
-            })
-        })
-    }, [])
-
-    useEffect(() => {
-        const queryInfo = { active: true, currentWindow: true }
-        chrome.tabs?.query(queryInfo, (tabs) => {
-            const ts = getTimeStampFromQuery(tabs[0])
-            if (ts) {
-                setDateByTimestamp(ts)
-            }
-        })
-    }, [])
-
     const setNextMonth = () => {
         const newDate = targetDate.clone().month(targetDate.get('month') + 1)
         setTargetDate(newDate)
