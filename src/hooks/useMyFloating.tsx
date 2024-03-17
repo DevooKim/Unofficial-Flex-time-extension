f70a0e61deb00c9c74f7230778b53a41f507a531
Devookim <khwoo8026@gmail.com>
2024-03-18 03:14:17 +0900

    feat: 스위치에 툴팁 추가

--- src/hooks/useMyFloating.tsx
+++ src/hooks/useMyFloating.tsx
@@ -4,22 +4,30 @@ import {
     Placement,
     arrow,
     offset as offsetBase,
+    useDelayGroup,
     useFloating as useFloatingBase,
     useFocus,
     useHover,
     useInteractions,
+    useTransitionStyles,
 } from '@floating-ui/react'
 import { useRef, useState } from 'react'
 
 type myFloatingType = {
     placement?: Placement
     offset?: OffsetOptions
-    useClientPoint?: boolean
+    delay?:
+        | number
+        | Partial<{
+              open: number
+              close: number
+          }>
 }
 
 const useMyFloating = ({
     placement = 'bottom',
     offset = 8,
+    delay = 0,
 }: myFloatingType) => {
     const [isOpen, setIsOpen] = useState(false)
     const arrowRef = useRef<SVGSVGElement>(null)
@@ -36,9 +44,14 @@ const useMyFloating = ({
         ],
     })
 
+    useDelayGroup(floating.context, {
+        id: floating.context.floatingId,
+    })
+
+    const floatingHover = useHover(floating.context, { delay })
+    const floatingFocus = useFocus(floating.context)
+
     const getFloatingInteraction = (hooks: Array<ElementProps | void> = []) => {
-        const floatingHover = useHover(floating.context)
-        const floatingFocus = useFocus(floating.context)
         const floatingInteraction = useInteractions([
             floatingHover,
             floatingFocus,
@@ -46,6 +59,17 @@ const useMyFloating = ({
         ])
 
         return floatingInteraction
+    }
+
+    const { styles } = useTransitionStyles(floating.context, {
+        duration: 250,
+        initial: {
+            opacity: 0,
+        },
+    })
+
+    if (delay) {
+        floating.floatingStyles = { ...floating.floatingStyles, ...styles }
     }
 
     return {
--- src/pages/popup/components/TimeDataSwitch/TimeDataSwitch.tsx
+++ src/pages/popup/components/TimeDataSwitch/TimeDataSwitch.tsx
@@ -1,7 +1,9 @@
+import { FloatingPortal, useHover } from '@floating-ui/react'
 import { useEffect, useState } from 'react'
 import Browser from 'webextension-polyfill'
 
 import ButtonToggleGroup from '@src/components/ButtonToggleGroup'
+import useMyFloating from '@src/hooks/useMyFloating'
 
 import TimeOff from './components/TimeOff'
 import WorkingRecord from './components/WorkingRecord'
@@ -10,6 +12,19 @@ const ButtonKeyList = ['근무', '휴가'] as const
 
 const TimeDataSwitch = () => {
     const [activeStatus, setActiveStatus] = useState<'근무' | '휴가'>('근무')
+
+    const { floating, getFloatingInteraction, isOpen } = useMyFloating({
+        placement: 'bottom',
+        delay: {
+            open: 750,
+        },
+        offset: ({ rects }) => ({
+            crossAxis: rects.floating.width - 8,
+            mainAxis: 4,
+        }),
+    })
+
+    const floatingInteraction = getFloatingInteraction()
 
     const toggle = () =>
         setActiveStatus((prev) => (prev === '근무' ? '휴가' : '근무'))
@@ -24,22 +39,37 @@ const TimeDataSwitch = () => {
 
     return (
         <>
-            <ButtonToggleGroup
-                fullWidth
-                defaultIndex={ButtonKeyList.indexOf(activeStatus)}
-            >
-                {ButtonKeyList.map((key) => (
-                    <ButtonToggleGroup.Item
-                        key={key}
-                        onClick={() => setActiveStatus(key)}
-                        value={key}
-                    >
-                        <div className="text-subtitle1">{key}</div>
-                    </ButtonToggleGroup.Item>
-                ))}
-            </ButtonToggleGroup>
+            <div ref={floating.refs.setReference}>
+                <ButtonToggleGroup
+                    fullWidth
+                    defaultIndex={ButtonKeyList.indexOf(activeStatus)}
+                >
+                    {ButtonKeyList.map((key) => (
+                        <ButtonToggleGroup.Item
+                            key={key}
+                            onClick={() => setActiveStatus(key)}
+                            value={key}
+                        >
+                            <div className="text-subtitle1">{key}</div>
+                        </ButtonToggleGroup.Item>
+                    ))}
+                </ButtonToggleGroup>
+            </div>
+
             {activeStatus === '근무' && <WorkingRecord />}
             {activeStatus === '휴가' && <TimeOff />}
+            <FloatingPortal>
+                {isOpen && (
+                    <div
+                        ref={floating.refs.setFloating}
+                        className="tooltip"
+                        style={floating.floatingStyles}
+                        {...floatingInteraction.getFloatingProps()}
+                    >
+                        Option + shift + E
+                    </div>
+                )}
+            </FloatingPortal>
         </>
     )
 }
