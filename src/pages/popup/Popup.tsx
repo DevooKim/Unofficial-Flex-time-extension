3a6657abb7f562441c4c1c74827eadcfd36a3825
lucalee-1 <30341309+lucalee-1@users.noreply.github.com>
2024-04-06 22:39:24 +0900

    feat: Popup width 시안 맞게 수정

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -27,7 +27,7 @@ export default function App(): JSX.Element {
 
     return (
         <QueryClientProvider client={queryClient}>
-            <div className="min-w-[350px] p-5">
+            <div className="min-w-[420px] p-5">
                 <QueryErrorResetBoundary>
                     {({ reset }) => (
                         <ErrorBoundary
