1b89410829c60b5ee3f0bc71a9cdb4c9e981c55b
Sain-Tech <chyh1900@gmail.com>
2024-04-11 00:05:46 +0900

    test: API 테스트 브랜치

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,5 +1,7 @@
 import Browser from 'webextension-polyfill'
 
+import { getCurrentSession } from '@src/utils/apis'
+
 console.log('Hello world, background service worker!')
 
 const fetchUserIdHash = async () => {
@@ -41,6 +43,87 @@ const fetchScheduleData = async ({
     )
 
     return await response.json()
+}
+
+const fetchMyCurrentStatus = async () => {
+    const userIdHash = await fetchUserIdHash()
+
+    const response = await fetch(
+        `https://flex.team/api/v2/time-tracking/work-clock/users/${userIdHash}/current-status`
+    )
+
+    const { targetDayWorkSchedule } = await response.json()
+    return targetDayWorkSchedule
+}
+
+const searchUsers = async ({
+    keyword,
+    filter,
+    sort,
+    size,
+    continuationToken,
+} = {}) => {
+    console.log('searching users...')
+    const makePagingQuery = ({ size, continuationToken } = {}) => {
+        const queries = []
+        if (size) {
+            queries.push(`size=${size}`)
+        }
+        if (continuationToken) {
+            queries.push(`continuationToken=${continuationToken}`)
+        }
+        return queries.length ? `?${queries.join('&')}` : ''
+    }
+    const response = await fetch(
+        `https://flex.team/action/v2/search/customers/pVEkBrmzMB/time-series/search-users${makePagingQuery({ size, continuationToken })}`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                ...(keyword && { keyword }),
+                ...(sort && { sort }),
+                filter: {
+                    jobTitleIdHashes: [...(filter?.jobTitleIdHashes ?? [])],
+                    jobRankIdHashes: [...(filter?.jobRankIdHashes ?? [])],
+                    jobRoleIdHashes: [...(filter?.jobRoleIdHashes ?? [])],
+                    departmentIdHashes: [...(filter?.departmentIdHashes ?? [])],
+                    headUsers: [...(filter?.headUsers ?? [])],
+                    userStatuses: [
+                        'LEAVE_OF_ABSENCE',
+                        'LEAVE_OF_ABSENCE_SCHEDULED',
+                        'RESIGNATION_SCHEDULED',
+                        'IN_EMPLOY',
+                        'IN_APPRENTICESHIP',
+                    ],
+                },
+                // sort: {
+                //     sortType: 'DISPLAY_NAME',
+                //     directionType: 'ASC',
+                // },
+            }),
+        }
+    )
+
+    console.log(await response.json())
+
+    return true
+}
+
+const fetchPrimaryWorkspaceCustomer = async () => {
+    const userIdHash = await fetchUserIdHash()
+    const response = await fetch(`
+    https://flex.team/api/v2/workspace/users/${userIdHash}/workspace-customers
+    `)
+
+    const { workspaceCustomers, count } = await response.json()
+
+    if (count === 1) {
+        return workspaceCustomers[0]
+    } else {
+        return workspaceCustomers.filter(({ isPrimary }) => isPrimary)[0] ?? []
+    }
 }
 
 // function sendNotification(notiId, msg, dwellingTimeMs) {
@@ -88,6 +171,18 @@ const fetchScheduleData = async ({
         timeStampTo: new Date('2024-02-28').getTime(),
     })
     console.log(scheduleData)
+
+    const myCurrentStatus = await fetchMyCurrentStatus()
+    console.log(myCurrentStatus)
+
+    const users = await searchUsers({ size: 100 })
+    console.log(users)
+
+    const primaryWorkspaceCustomer = await fetchPrimaryWorkspaceCustomer()
+    console.log(primaryWorkspaceCustomer)
+
+    const currentSession = await getCurrentSession()
+    console.log(currentSession)
 })()
 
 Browser.commands.onCommand.addListener((command) => {
