3957f174b10b2b8187ea625cfa87d5007a9bda9e
lucalee <30341309+lucalee-1@users.noreply.github.com>
2024-04-14 11:24:33 +0900

    refactor: 컴포넌트 분리

--- src/pages/popup/components/TimeDataSwitch/components/WorkingRecord/WorkingRecord.tsx
+++ src/pages/popup/components/TimeDataSwitch/components/WorkingRecord/WorkingRecord.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import IconButton from '@src/components/IconButton'
 import {
     useFetchClockData,
@@ -11,7 +13,93 @@ import { parseClockData } from '@src/utils/parseClockData'
 import { parseScheduleData } from '@src/utils/parseScheduleData'
 import { hourToString } from '@src/utils/utils.time'
 
-// TODO: *refactor* 필요한 데이터만 가져오기, 컴포넌트 분리
+const ProgressBar = ({
+    근무시간총합,
+    최소근무시간,
+    남은근무시간,
+}: {
+    근무시간총합: number
+    최소근무시간: number
+    남은근무시간: number
+}) => {
+    const [viewWorkedTime, setViewWorkedTime] = useState(true)
+
+    const progressBarWidth = `${Math.max(Math.min((근무시간총합 / 최소근무시간) * 100, 100), 3).toFixed(2)}%`
+
+    return (
+        <>
+            <div className="mb-1 flex items-center justify-between">
+                <h4 className="text-xl font-bold">
+                    {viewWorkedTime
+                        ? hourToString(근무시간총합)
+                        : `${hourToString(남은근무시간)} 남음`}
+
+                    <IconButton
+                        className="ml-1 h-6 w-6 rounded-lg bg-gray-100"
+                        icon={<TransferIcon />}
+                        onClick={() => setViewWorkedTime((prev) => !prev)}
+                    />
+                </h4>
+                <p className="m-0 mb-2 text-xs text-hint">{최소근무시간}시간</p>
+            </div>
+            <div className="h-2 w-full rounded bg-gray-200">
+                <div
+                    className="h-2 rounded bg-blue-500"
+                    style={{ width: progressBarWidth || 0 }}
+                />
+            </div>
+        </>
+    )
+}
+const TimeCard = ({
+    icon,
+    title,
+    text,
+}: {
+    icon: string
+    title: string
+    text: string
+}) => (
+    <div className="flex items-center gap-5 px-4 py-3">
+        <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-gray-100 ">
+            <h3
+                className="text-2xl/9 font-bold"
+                style={{
+                    textShadow: '-1px 2px 2px rgba(173, 173, 180, 1)',
+                }}
+            >
+                {icon}
+            </h3>
+        </div>
+        <div className="flex flex-col">
+            <p className="text-hint">{title}</p>
+            <h4 className="text-xl">{text}</h4>
+        </div>
+    </div>
+)
+
+const Skeleton = () => (
+    <div>
+        <div className="p-4">
+            <div className="mb-2 flex text-xs text-hint">
+                이번 달 최소 근무 시간
+                <HelpCircleIcon className="ml-1" />
+            </div>
+            <ProgressBar 근무시간총합={0} 최소근무시간={0} 남은근무시간={0} />
+        </div>
+        <TimeCard
+            icon="⌛"
+            title="지금 퇴근하면?"
+            text={'0시간 0분 남았어요.'}
+        />
+        <TimeCard
+            icon="👍"
+            title="오늘 권장 근무 시간은?"
+            text={'0시간 0분 정도 일하면 좋아요.'}
+        />
+    </div>
+)
+
 const WorkingRecord = () => {
     const { data: userIdHash } = useFetchUserIdHash()
     const { targetTimeStamp } = useGetTargetDate()
@@ -26,18 +114,21 @@ const WorkingRecord = () => {
         timeStampTo: lastDay,
     })
 
-    if (scheduleLoading || clockLoading) return <div />
+    if (scheduleLoading || clockLoading) return <Skeleton />
 
     const myClockData = parseClockData({ data: clockData, now })
 
-    const { 근무시간총합, 최소근무시간, 지금기준, 남은평균근무시간 } =
-        parseScheduleData({
-            data: scheduleData,
-            today: baseTimeData.today,
-            clockData: myClockData,
-        })
-
-    const progressBarWidth = `${Math.max(Math.min((근무시간총합 / 최소근무시간) * 100, 100), 3).toFixed(2)}%`
+    const {
+        근무시간총합,
+        최소근무시간,
+        지금기준,
+        남은평균근무시간,
+        남은근무시간,
+    } = parseScheduleData({
+        data: scheduleData,
+        today: baseTimeData.today,
+        clockData: myClockData,
+    })
 
     return (
         <div>
@@ -46,61 +137,22 @@ const WorkingRecord = () => {
                     이번 달 최소 근무 시간
                     <HelpCircleIcon className="ml-1" />
                 </div>
-                <div className="mb-1 flex items-center justify-between">
-                    <h4 className="text-xl font-bold">
-                        {hourToString(근무시간총합)}{' '}
-                        <IconButton
-                            className="h-6 w-6 rounded-lg bg-gray-100"
-                            icon={<TransferIcon />}
-                        />
-                    </h4>
-                    <p className="m-0 mb-2 text-xs text-hint">
-                        {최소근무시간}시간
-                    </p>
-                </div>
-                <div className="h-2 w-full rounded bg-gray-200">
-                    <div
-                        className="h-2 rounded bg-blue-500"
-                        style={{ width: progressBarWidth || 0 }}
-                    />
-                </div>
+                <ProgressBar
+                    근무시간총합={근무시간총합}
+                    최소근무시간={최소근무시간}
+                    남은근무시간={남은근무시간}
+                />
             </div>
-            <div className="flex items-center gap-5 px-4 py-3">
-                <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-gray-100 ">
-                    <h3
-                        className="text-2xl/9 font-bold"
-                        style={{
-                            textShadow: '-1px 2px 2px rgba(173, 173, 180, 1)',
-                        }}
-                    >
-                        ⌛
-                    </h3>
-                </div>
-                <div className="flex flex-col">
-                    <p className="text-hint">지금 퇴근하면?</p>
-                    <h4 className="text-xl">
-                        {hourToString(지금기준.남은근무시간)} 남았어요.
-                    </h4>
-                </div>
-            </div>
-            <div className="flex items-center gap-5 px-4 py-3">
-                <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-gray-100 ">
-                    <h3
-                        className="text-2xl/9 font-bold"
-                        style={{
-                            textShadow: '-1px 2px 2px rgba(173, 173, 180, 1)',
-                        }}
-                    >
-                        👍
-                    </h3>
-                </div>
-                <div className="flex flex-col">
-                    <p className="text-hint">오늘 권장 근무 시간은?</p>
-                    <h4 className="text-xl">
-                        {hourToString(남은평균근무시간)} 정도 일하면 좋아요.
-                    </h4>
-                </div>
-            </div>
+            <TimeCard
+                icon="⌛"
+                title="지금 퇴근하면?"
+                text={`${hourToString(지금기준.남은근무시간)} 남았어요.`}
+            />
+            <TimeCard
+                icon="👍"
+                title="오늘 권장 근무 시간은?"
+                text={`${hourToString(남은평균근무시간)} 정도 일하면 좋아요.`}
+            />
         </div>
     )
 }
